fc82150ebe9fb59f9e70c824218e54465408b7c5
root-lucas <945832032@qq.com>
2020-03-08 08:29:45 +0800

    login: add 单元测试-验证 Model

---
+++ jest-test/user/model.test.js
@@ -0,0 +1,26 @@
+/**
+ * @description user model test
+ * @author lucas
+ */
+
+const { User } = require('../../src/db/model/index')
+
+test('User 模型的各个属性，符合预期', () => {
+    // build 会构建一个内存的 User 实例，但不会提交到数据库中
+    const user = User.build({
+        userName: 'zhangsan',
+        password: 'p123123',
+        nickName: '张三',
+        // gender: 1,
+        picture: '/xxx.png',
+        city: '广州'
+    })
+
+    // 验证各个属性
+    expect(user.userName).toBe('zhangsan')
+    expect(user.password).toBe('p123123')
+    expect(user.nickName).toBe('张三')
+    expect(user.gender).toBe(3) // 测试 gender 默认值 # 上面没有设置所以从/model/index文件去找 defaultValue: 3
+    expect(user.picture).toBe('/xxx.png')
+    expect(user.city).toBe('广州')
+})
--- src/utils/env.js
+++ src/utils/env.js
@@ -3,7 +3,7 @@
  * @author lucas
  */
 
-const ENV = process.NODE_ENV
+const ENV = process.env.NODE_ENV
 
 module.exports = {
     isDev: ENV === 'dev',
