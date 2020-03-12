7d8e46847a37bb5d4819fdc560f80c083cda9ea3
root-lucas <945832032@qq.com>
2020-03-13 02:33:20 +0800

    blog: feat 微博用户模型

--- src/db/model/index.js
+++ src/db/model/index.js
@@ -4,7 +4,14 @@
  */
 
 const User = require('./User')
+const Blog = require('./Blog')
+
+// 查询微博的时候顺带查询用户
+Blog.belongsTo(User, {
+    foreignKey: 'userId'
+})
 
 module.exports = {
-    User
+    User,
+    Blog
 }
