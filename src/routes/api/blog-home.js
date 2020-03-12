2ebc62421fee39bc37cf599f4b0564561f46443c
root-lucas <945832032@qq.com>
2020-03-13 05:11:52 +0800

    blog: feat xss过滤和schema格式校验

--- src/routes/api/blog-home.js
+++ src/routes/api/blog-home.js
@@ -6,14 +6,21 @@
 const router = require('koa-router')()
 const { loginCheck } = require('../../middlewares/loginChecks')
 const { create } = require('../../controller/blog-home')
+const genValidator = require('../../middlewares/validator')
+const blogValidate = require('../../validator/blog')
 
 router.prefix('/api/blog')
 
 // 创建微博
-router.post('/create', loginCheck, async (ctx, next) => {
-    const { content, image } = ctx.request.body
-    const { id: userId } = ctx.session.userInfo // userInfo.id 改别名为 userId
-    ctx.body = await create({ userId, content, image })
-})
+router.post(
+    '/create',
+    loginCheck,
+    genValidator(blogValidate),
+    async (ctx, next) => {
+        const { content, image } = ctx.request.body
+        const { id: userId } = ctx.session.userInfo // userInfo.id 改别名为 userId
+        ctx.body = await create({ userId, content, image })
+    }
+)
 
 module.exports = router
