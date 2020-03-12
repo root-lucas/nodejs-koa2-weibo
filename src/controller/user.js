3c81666c22c3809659f1f4e87a8cfa1c103d7f1b
root-lucas <945832032@qq.com>
2020-03-12 09:17:30 +0800

    setting: feat API-修改基本信息

--- src/controller/user.js
+++ src/controller/user.js
@@ -3,14 +3,20 @@
  * @author lucas
  */
 
-const { getUserInfo, createUser, deleteUser } = require('../services/user')
+const {
+    getUserInfo,
+    createUser,
+    deleteUser,
+    updateUser
+} = require('../services/user')
 const { SuccessModel, ErrorModel } = require('../model/ResModel')
 const {
     registerUserNameNotExistInfo,
     registerUserNameExistInfo,
     registerFailInfo,
     loginFailInfo,
-    deleteUserFailInfo
+    deleteUserFailInfo,
+    changeInfoFailInfo
 } = require('../model/ErrorInfo')
 const doCrypto = require('../utils/cryp')
 
@@ -92,9 +98,45 @@ async function deleteCurUser(userName) {
     return new ErrorModel(deleteUserFailInfo)
 }
 
+/**
+ * 修改个人信息
+ * @param {Object} ctx ctx
+ * @param {string} nickName 昵称
+ * @param {string} city 城市
+ * @param {string} picture 头像
+ */
+async function changeInfo(ctx, { nickName, city, picture }) {
+    const { username } = ctx.session.userInfo
+    if (!nickName) {
+        nickName = username
+    }
+
+    const result = await updateUser(
+        {
+            newNickName: nickName,
+            newCity: city,
+            newPicture: picture
+        },
+        { username }
+    )
+    if (result) {
+    // 执行成功, 更新 session 数据
+        Object.assign(ctx.session.userInfo, {
+            nickName,
+            city,
+            picture
+        })
+        // 返回
+        return new SuccessModel()
+    }
+    // 失败
+    return new ErrorModel(changeInfoFailInfo)
+}
+
 module.exports = {
     isExist,
     register,
     login,
-    deleteCurUser
+    deleteCurUser,
+    changeInfo
 }
--- src/routes/api/user.js
+++ src/routes/api/user.js
@@ -9,7 +9,8 @@ const {
     isExist,
     register,
     login,
-    deleteCurUser
+    deleteCurUser,
+    changeInfo
 } = require('../../controller/user')
 const userValidate = require('../../validator/user')
 const genValidator = require('../../middlewares/validator')
@@ -44,5 +45,16 @@ router.post('/delete', loginCheck, async (ctx, next) => {
         ctx.body = await deleteCurUser(username)
     }
 })
+
+// 修改个人信息
+router.patch(
+    '/changeInfo',
+    loginCheck,
+    genValidator(userValidate),
+    async (ctx, next) => {
+        const { nickName, city, picture } = ctx.request.body
+        ctx.body = await changeInfo(ctx, { nickName, city, picture })
+    }
+)
 
 module.exports = router
