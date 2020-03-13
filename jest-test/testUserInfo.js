349cd1898d05c59e7c7babb8f62ed388c9d440b1
root-lucas <945832032@qq.com>
2020-03-13 22:47:46 +0800

    blog: test 创建微博单元测试

---
+++ jest-test/testUserInfo.js
@@ -0,0 +1,14 @@
+/**
+ * @description 单元测试的用户信息
+ * @author lucas
+ */
+
+/**
+ * 【特别提醒】cookie 是用户的敏感信息，此处只能是**测试**用户的 cookie
+ * 每次测试用户重新登录，都需要更新这里的 cookie
+ */
+
+module.exports = {
+    COOKIE:
+    'weibo.sid=uNq2phGA6GhzhDiqPzWoknNMqc0lyjMT; weibo.sid.sig=etlKudpVd09ShSG_eHRU6cvqKFY'
+}
