54408222e4ed9b316e6f48417e798d05abcbe66b
root-lucas <945832032@qq.com>
2020-03-19 10:03:47 +0800

    home: test 单元测试

--- jest-test/testUserInfo.js
+++ jest-test/testUserInfo.js
@@ -10,14 +10,14 @@
 
 module.exports = {
     // 张三
-    Z_ID: 2, // 此处对应数据库表的 id 字段
+    Z_ID: 1, // 此处对应数据库表的 id 字段
     Z_USER_NAME: 'zhangsan',
     Z_COOKIE:
     'weibo.sid=REOa5gPDwNwkNVEkq-KPn96eFIwTSVpt; weibo.sid.sig=QHORdFRvCB2_UDXAjr0WAWG_VQA',
 
     // 李四
-    L_ID: 1,
+    L_ID: 2,
     L_USER_NAME: 'lisi',
     L_COOKIE:
-    'weibo.sid=0Qeg9hVPqA5MfFObrsGBEVIiDK89itRI; weibo.sid.sig=wH3_yb6fFS6nLXqJYMsXasC9_yk'
+    'weibo.sid=urXFvj844UuyGjV6vV1raD-8FuMQ85tH; weibo.sid.sig=ngTHlkSY16JrPj72p2wwIwiJOqU'
 }
